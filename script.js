dcff916ce5123a93d875c5ef0115f1024c4d9ebb
Inkwax <147993308+Inkwax@users.noreply.github.com>
2026-02-01 22:05:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const TYPES = {
+  Normal:   { color: "#f5f5ee" }, // off white
+  Fire:     { color: "#e53935" }, // red
+  Water:    { color: "#1e88e5" }, // blue
+  Electric: { color: "#fdd835" }, // yellow
+  Grass:    { color: "#43a047" }, // green
+  Ice:      { color: "#26c6da" }, // cyan
+  Fighting: { color: "#fb8c00" }, // orange
+  Poison:   { color: "#8e24aa" }, // purple
+  Ground:   { color: "#ffb300" }, // amber
+  Flying:   { color: "#81d4fa" }, // light blue
+  Psychic:  { color: "#ec407a" }, // magenta
+  Bug:      { color: "#c0ca33" }, // lime
+  Rock:     { color: "#8d6e63" }, // brown
+  Ghost:    { color: "#5e35b1" }, // indigo
+  Dragon:   { color: "#1b5e20" }, // dark green
+  Dark:     { color: "#212121" }, // off black
+  Steel:    { color: "#d84315" }, // vermillion
+  Fairy:    { color: "#f48fb1" }  // pink
+};
 
+const TYPE_LIST = Object.keys(TYPES);
